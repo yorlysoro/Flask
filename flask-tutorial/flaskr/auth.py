506a37c79480c2a82bee59f011dffb2e07d0cf1a
yorlysoro <yorlysoro@gmail.com>
2020-06-07 16:26:26 -0400

    Tutorial Flask Templates

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -4,11 +4,11 @@ from flask import (
 	Blueprint, flash, g, redirect, render_template, request, session, url_for
 	)
 from werkzeug.security import check_password_hash, generate_password_hash
-from flaksr.db import get_db
+from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@db.route('/register', methods=('GET', 'POST'))
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
 	if request.method == 'POST':
 		username = request.form['username']
